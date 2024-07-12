bb1bc2a1d3dab5dfa86f933c0353b274f94bf094
erase1657 <68234764+erase1657@users.noreply.github.com>
2024-07-12 23:03:13 +0900

    [Bronze II] Title: 단어의 개수, Time: 192 ms, Memory: 26020 KB -BaekjoonHub

--- 백준/Bronze/1152. 단어의 개수/단어의 개수.java
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -5,15 +5,17 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine()," ");
 
-        int count  = st.countTokens();
-        String[] word = new String[count];
+        int count  = st.countTokens(); // 토큰의 갯수를 카운트
+        String[] word = new String[count]; //카운트 한 수만큼의 배열 선언 
         int i = 0;
         
+        //토큰이 존재하지 않을 때 까지
+        //토큰을 배열에 하나씩 넣어가며 i(단어의 수)를 하나씩 증가
         while(st.hasMoreTokens()){
             word[i] = st.nextToken();
             i++;
         }
-        
+         
         System.out.println(i);
     }
 }

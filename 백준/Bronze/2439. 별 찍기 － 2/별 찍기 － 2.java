804e0f1daeee912392c8d3df28ee489dc1bbed48
erase1657 <68234764+erase1657@users.noreply.github.com>
2024-07-23 22:16:48 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 76 ms, Memory: 11792 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.java
@@ -0,0 +1,31 @@
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        
+     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));  
+     BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+     int num = Integer.parseInt(br.readLine());
+    
+     for(int i=1;i<=num;i++){
+        
+        for(int j=num;j>i;j--){
+            bw.write(" ");
+        }
+        for(int j=1; j<=i;j++){
+            bw.write("*");
+        }
+        
+        bw.newLine();
+     }
+     bw.flush();
+     bw.close();
+     
+    }
+}
